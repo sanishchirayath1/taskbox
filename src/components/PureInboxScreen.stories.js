1c34684d2dd82d5123bdc06253cf043242d0aa6a
sanishchirayath1 <sanishchirayath@gmail.com>
2023-03-08 14:22:12 +0530

    feat: created build files

---
+++ src/components/PureInboxScreen.stories.js
@@ -0,0 +1,38 @@
+import { app } from "@storybook/vue3";
+
+import { fireEvent, within } from "@storybook/testing-library";
+
+import { createPinia } from "pinia";
+
+app.use(createPinia());
+
+import PureInboxScreen from "./PureInboxScreen.vue";
+
+export default {
+  title: "PureInboxScreen",
+  component: PureInboxScreen,
+};
+
+const Template = (args) => ({
+  components: { PureInboxScreen },
+  setup() {
+    return {
+      args,
+    };
+  },
+  template: '<PureInboxScreen v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+
+export const Error = Template.bind({});
+Error.args = { error: true };
+
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+};
